667e6bf157c7df67f814586a03ed0a8670ea7668
groverohit <groverohit@gmail.com>
2020-07-17 10:36:23 -0700

    Implement alerts following the OOP Paradigm

--- src/NumberofEvents.js
+++ src/NumberofEvents.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberofEvents extends Component {
-  state = { numberOfEvents: 32 };
+  state = { numberOfEvents: 32, errorText: "" };
 
   handleOnChange = (event) => {
     const value = event.target.value;
+    if (value < 1) this.setState({ errorText: "Please enter a valid number" });
+    else this.setState({ errorText: "" });
     this.setState({ numberOfEvents: value });
     this.props.updateEvents(null, null, value);
   };
@@ -12,6 +15,7 @@ class NumberofEvents extends Component {
   render() {
     return (
       <div className="numberOfEvents">
+        <ErrorAlert text={this.state.errorText} />
         <label>Number of Events: </label>
         <input
           value={this.state.numberOfEvents}
